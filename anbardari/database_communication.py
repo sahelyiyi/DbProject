49a5837b06a900b18434c1cd0e67b2346edd0090
sahel <sahel@cafebazaar.ir>
2017-08-13 16:54:31 +0430

    work with database

---
+++ anbardari/database_communication.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+
+c = conn.cursor()
+
+c.execute('''CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real)''')
+
+purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
+             ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
+             ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+            ]
+c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
+
+for row in c.execute('SELECT * FROM stocks ORDER BY price'):
+        print row
