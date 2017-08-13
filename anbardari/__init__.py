91d97cb0c02cd19c07445aefb9455a4edba5c808
sahel <sahel@cafebazaar.ir>
2017-08-13 17:38:25 +0430

    add goods information

---
+++ anbardari/__init__.py
@@ -0,0 +1,15 @@
+entities = {
+    'goods': {
+        'group_title': 'text',
+        'base_price': 'real',
+        'maintenance': 'text',
+        'barcode': 'integer',
+        'code': 'integer',
+        'exit_time': 'numeric',
+        'entry_time': 'numeric',
+        'production_time': 'numeric',
+        'producer': 'text',
+        'price': 'real',
+        'name': 'text'
+    }
+}
