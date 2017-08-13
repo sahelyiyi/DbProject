ad27fc84e95f1de71affe419423608b3f5ec26a8
sahel <sahel@sarcheshmehpurs@gmail.com>
2017-08-13 22:37:40 +0430

    add some needs

---
+++ anbardari/member.py
@@ -0,0 +1,18 @@
+def order(member_code):
+    pass
+
+def deliver(member_code):
+    pass
+
+def take_delivery(member_code):
+    pass
+
+def edit_info(member_code):
+    pass
+
+def get_goods(member_code):
+    pass
+
+
+def calculate_keep_price_(member_code):
+    pass
---
+++ anbardari/staff.py
@@ -0,0 +1,8 @@
+def add_goods_info(*goods_info):
+    pass
+
+def add_exit_date(goods_barcode, exit_date):
+    pass
+
+def get_salary(national_code):
+    pass
