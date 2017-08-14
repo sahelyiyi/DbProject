4813755ccc5494f34aec7ec42f3dae5a67c2f4c6
sahel <sahel@cafebazaar.ir>
2017-08-14 11:22:21 +0430

    refactor

--- anbardari/database_communication.py
+++ anbardari/database_communication.py
@@ -17,15 +17,3 @@ def create_table(table_name, table_info):
     create_str = create_str.replace(']', ')')
     c.execute(create_str)
 
-
-# c.execute('''CREATE TABLE stocks
-#              (date text, trans text, symbol text, qty real, price real)''')
-#
-# purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
-#              ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
-#              ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
-#             ]
-# c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
-#
-# for row in c.execute('SELECT * FROM stocks ORDER BY price'):
-#         print row
